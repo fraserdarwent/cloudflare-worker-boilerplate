6a835baa823eff18b99ed0bad300f6226347a90a
Fraser Darwent <fraser@darwen.tf>
2020-09-11 10:42:35 +0100

    Prettier

--- src/main.js
+++ src/main.js
@@ -1,25 +1,25 @@
-import log from '@fraserdarwent/javascript-logger'
-import { pathToRegexp } from 'path-to-regexp'
-import methods from './methods.js'
+import log from '@fraserdarwent/javascript-logger';
+import {pathToRegexp} from 'path-to-regexp';
+import methods from './methods.js';
 
 if (env.LOG_LEVEL) {
-  log.setLevel(log.levels[env.LOG_LEVEL])
+  log.setLevel(log.levels[env.LOG_LEVEL]);
 }
 
 const apis = {
   health: pathToRegexp('/health'),
-}
+};
 
 const headers = {
   'Content-Type': 'application/json',
   'Access-Control-Allow-Origin': env.CORS,
   'Access-Control-Allow-Headers': 'Content-Type',
   'Access-Control-Allow-Methods': '*',
-}
+};
 
-addEventListener('fetch', (event) => {
-  event.respondWith(handleRequest(event.request))
-})
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request));
+});
 
 /**
  * @param {Request} request
@@ -27,20 +27,20 @@ addEventListener('fetch', (event) => {
  */
 async function handleRequest(request) {
   try {
-    const url = new URL(request.url)
-    const path = url.pathname
+    const url = new URL(request.url);
+    const path = url.pathname;
 
-    log.debug(`${path}`)
+    log.debug(`${path}`);
 
     switch (true) {
       case apis.health.test(path):
-        return health(request)
+        return health(request);
       default:
-        return json({ errors: [{ message: 'Page not found' }] }, 404)
+        return json({errors: [{message: 'Page not found'}]}, 404);
     }
   } catch (error) {
-    log.error(error)
-    return json({ errors: [{ message: 'Internal server error' }] }, 500)
+    log.error(error);
+    return json({errors: [{message: 'Internal server error'}]}, 500);
   }
 }
 
@@ -54,7 +54,7 @@ function json(body = {}, status = 200) {
   return new Response(JSON.stringify(body), {
     status: status,
     headers: headers,
-  })
+  });
 }
 
 /**
@@ -64,10 +64,10 @@ function json(body = {}, status = 200) {
 function health(request) {
   switch (request.method) {
     case methods.get:
-      return json({ status: 'pass' })
+      return json({status: 'pass'});
     case methods.options:
-      return options()
+      return options();
     default:
-      return json({ errors: [{ message: 'Method not allowed' }] }, 405)
+      return json({errors: [{message: 'Method not allowed'}]}, 405);
   }
 }
